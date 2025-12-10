cd113017e003ce68c355a777964dcaa108de1e85
Rafi Arrafif <rafiarrafif05@gmail.com>
2025-12-10 23:41:55 +0700

    :heavy_plus_sign: (dep-add) hero ui library and prerequisites settings.

---
+++ providers/HeroUIWrapper.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { HeroUIProvider } from "@heroui/react";
+import React from "react";
+
+const HeroUIWrapper = ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <div>
+      <HeroUIProvider>{children}</HeroUIProvider>
+    </div>
+  );
+};
+
+export default HeroUIWrapper;
