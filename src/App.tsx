3e7426d3f9e0b3c0f7021b9f528b307d525d95be
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-01-12 14:38:36 +0700

    ✨ feat: mainframe of about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import About from "./sections/about/About";
 import Hero from "./sections/hero/Hero";
 import Navbar from "./sections/navbar/Navbar";
 
 const App = () => {
   return (
     <div>
-      <div className="border-b border-b-neutral-200">
+      <div className="border-b border-b-neutral-200 fixed top-0 left-0 w-full bg-white z-50">
         <Navbar />
       </div>
-      <div>
+      <div className="pt-12">
         <Hero />
+      </div>
+      <div>
+        <About />
       </div>
     </div>
   );
---
+++ src/sections/about/About.tsx
@@ -0,0 +1,43 @@
+const About = () => {
+  return (
+    <div className="bg-[#FBF9FF]">
+      <div>
+        <div>
+          <h1>Tentang Kemenko</h1>
+          <img src="/logo/Medtek.png" alt="logo medtek" />
+        </div>
+        <div />
+        <div>
+          <p>
+            Kemenko Media dan Teknologi adalah garda terdepan informasi dan
+            digitalisasi BEM FATISDA. Kami tidak hanya menyampaikan berita,
+            tetapi kami merancang pengalaman.
+          </p>
+          <p>
+            Dari visual yang memukau hingga sistem teknologi yang efisien, kami
+            hadir untuk menghubungkan mahasiswa dengan birokrasi melalui cara
+            yang kreatif dan inovatif.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img src="/logo/Medkom.png" alt="Logo media dan komunikasi" />
+          <img src="/logo/Pemtek.png" alt="Logo pengembangan teknologi" />
+        </div>
+        <div>
+          <div>
+            <h1>15</h1>
+            <h4>Program Kerja</h4>
+          </div>
+          <div>
+            <h1>15</h1>
+            <h3>Peserta Maganger</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
