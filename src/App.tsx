d266ffd6b3af3bb66d68b95f319675320f8ccdb4
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-01-12 11:02:34 +0700

    ✨ feat: hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Hero from "./sections/hero/Hero";
 import Navbar from "./sections/navbar/Navbar";
 
 const App = () => {
   return (
-    <div className="border-b border-b-neutral-200">
-      <Navbar />
+    <div>
+      <div className="border-b border-b-neutral-200">
+        <Navbar />
+      </div>
+      <div>
+        <Hero />
+      </div>
     </div>
   );
 };
