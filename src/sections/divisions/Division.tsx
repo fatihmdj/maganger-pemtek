7da87e6c69a448b5c495351d0a1127386ee174ae
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-01-15 01:21:32 +0700

    💫 feature: create basic animation in division's logo

--- src/sections/divisions/Division.tsx
+++ src/sections/divisions/Division.tsx
@@ -1,6 +1,40 @@
+import { useEffect } from "react";
+import DataDivision from "../../data/divisions.json";
 import { Icon } from "@iconify/react";
+import gsap from "gsap";
 
 const Division = () => {
+  useEffect(() => {
+    const parents = gsap.utils.toArray(
+      ".division-parent-card"
+    ) as HTMLElement[];
+
+    parents.forEach((parent) => {
+      const imageCard = parent.querySelector(
+        ".division-image-card"
+      ) as HTMLElement;
+      parent.addEventListener("mouseenter", () => {
+        gsap.to(imageCard, {
+          rotate: 90,
+          top: "2rem",
+          left: "28rem",
+          scale: 0.8,
+          duration: 0.5,
+          ease: "back.out(1.3)",
+        });
+      });
+      parent.addEventListener("mouseleave", () => {
+        gsap.to(imageCard, {
+          rotate: 0,
+          top: "3rem",
+          left: "3rem",
+          scale: 1,
+          duration: 0.5,
+          ease: "back.out(1.3)",
+        });
+      });
+    });
+  }, []);
   return (
     <div className="pt-32 pb-30">
       <div className="text-center pb-16">
@@ -11,55 +45,32 @@ const Division = () => {
           Dua pilar utama penggerak inovasi BEM FATISDA
         </h3>
       </div>
-      <div className="flex justify-center gap-16">
-        <div className="border border-neutral-200 pb-8 pt-12 pl-12 w-150 rounded-3xl">
-          <img
-            src="/icons/MedkomIcon.png"
-            alt="icon-medkom-bem-fatisda-uns"
-            className="w-auto h-34"
-          />
-          <div className="flex flex-col gap-2 mt-10">
-            <h1 className="text-6xl font-black tracking-tight text-neutral-900">
-              Medkom
-            </h1>
-            <h3 className="text-lg font-medium text-neutral-500">
-              "The Voice & The Vision"
-            </h3>
+      <div className="flex justify-center gap-8">
+        {DataDivision.map((division, index) => (
+          <div
+            key={index}
+            className="relative border border-neutral-200 w-150 h-105 rounded-3xl division-parent-card"
+          >
+            <img
+              src={division.image}
+              alt="icon-medkom-bem-fatisda-uns"
+              className="w-auto h-34 absolute top-12 left-12 division-image-card"
+            />
+            <div className="flex flex-col gap-2 absolute bottom-24 left-12">
+              <h1 className="text-6xl font-black tracking-tight text-neutral-900">
+                {division.name}
+              </h1>
+              <h3 className="text-lg font-medium text-neutral-500">
+                "{division.tagline}"
+              </h3>
+            </div>
+            <p className="hidden">{division.description}</p>
+            <div className="text-neutral-400 text-lg flex items-center gap-2 absolute bottom-8 left-12">
+              <Icon icon={division.icon} />
+              <span className="font-medium">{division.focus}</span>
+            </div>
           </div>
-          <p className="hidden">
-            Bertanggung jawab atas branding, desain grafis, videografi, dan
-            manajemen media sosial. Mereka adalah pencerita yang memastikan
-            setiap pesan BEM terdengar lantang dan estetik.
-          </p>
-          <div className="text-neutral-400 text-lg flex items-center gap-2 mt-8">
-            <Icon icon="fluent:megaphone-12-filled" />
-            <span className="font-medium">Social Media & Branding</span>
-          </div>
-        </div>
-        <div className="border border-neutral-200 pb-8 pt-12 pl-12 w-150 rounded-3xl">
-          <img
-            src="/icons/PemtekIcon.png"
-            alt="icon-pemtek-bem-fatisda-uns"
-            className="w-auto h-34"
-          />
-          <div className="flex flex-col gap-2 mt-10">
-            <h1 className="text-6xl font-black tracking-tight text-neutral-900">
-              Pemtek
-            </h1>
-            <h3 className="text-lg font-medium text-neutral-500">
-              "The Architects of Digital Ecosystem"
-            </h3>
-          </div>
-          <p className="hidden">
-            Bertanggung jawab atas branding, desain grafis, videografi, dan
-            manajemen media sosial. Mereka adalah pencerita yang memastikan
-            setiap pesan BEM terdengar lantang dan estetik.
-          </p>
-          <div className="text-neutral-400 text-lg flex items-center gap-2 mt-8">
-            <Icon icon="mingcute:code-fill" className="text-xl" />
-            <span className="font-medium">Web & App Development</span>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
