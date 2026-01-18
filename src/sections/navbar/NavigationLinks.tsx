eed40611fc0aca138d9dd42617d7cf25dfe6fa8b
Rafi Arrafif <rafiarrafif05@gmail.com>
2026-01-18 22:51:41 +0700

    feat: navbar for mobile layout

--- src/sections/navbar/NavigationLinks.tsx
+++ src/sections/navbar/NavigationLinks.tsx
@@ -1,6 +1,8 @@
-const NavigationLinks = () => {
+const NavigationLinks = ({ isMobile = false }: { isMobile?: boolean }) => {
   return (
-    <div className="font-medium text-lg space-x-8 text-neutral-900">
+    <div
+      className={`${isMobile ? "flex flex-col gap-4 text-lg text-neutral-900" : "font-medium text-lg space-x-8 text-neutral-900"}`}
+    >
       <a href="#about">About</a>
       <a href="#divisions">Divisions</a>
       <a href="#proker">Proker</a>
